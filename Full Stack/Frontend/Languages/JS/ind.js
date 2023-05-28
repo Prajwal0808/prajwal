7d3fdb3135ec605e98eaf5ba0157fc92a98c3015
Prajwal0808 <127934995+Prajwal0808@users.noreply.github.com>
2023-05-28 11:01:45 +0530

    Add files via upload

---
+++ Full Stack/Frontend/Languages/JS/ind.js
@@ -0,0 +1,28 @@
+function add(val1,val2){
+
+return parseInt(val1) + parseInt(val2)
+
+}
+console.log (add(218,216));
+
+
+function mul(val1,val2){
+
+    return parseInt(val1) * parseInt(val2)
+    
+    }
+    console.log (mul(218,216));
+
+    function div(val1,val2){
+
+        return parseInt(val1) / parseInt(val2)
+        
+        }
+        console.log (div(218,216));
+
+        function sub(val1,val2){
+
+            return parseInt(val1) - parseInt(val2)
+            
+            }
+            console.log (sub(218,216));
---
+++ Full Stack/Frontend/Languages/JS/index.js
@@ -0,0 +1,7 @@
+let name="prajwal";
+let num=146562;
+console.log(name);
+var accno=87574;
+console.log(accno);
+const name1="prajwal"
+console.log(name);
